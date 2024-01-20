79173c5f0de1e5be0e86d0d82734cad7df65ef4e
Aqil <mdaqil7300@gmail.com>
2024-01-20 11:12:49 +0530

    created custom hooks

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import apiClient from "../services/api-client";
+
+interface Game {
+    id: number;
+    name: string;
+}
+interface FetchGameResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGame] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        apiClient.get<FetchGameResponse>('/games')
+            .then(res => setGame(res.data.results))
+            .catch(err => setError(err.message))
+    }, [])
+    return { games, error }
+}
+
+export default useGames
